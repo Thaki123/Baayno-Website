83e1c7c6eb3fe6f3780af5970ef1d70c0c07c7f8
Alpha978 <83167565+Thaki123@users.noreply.github.com>
2025-08-22 22:08:43 +0300

    Switch to SSR hosting

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 import nextPWA from 'next-pwa';
 import nextI18NextConfig from './next-i18next.config.js';
 
-// Configure base path and asset prefix for GitHub Pages deployments
 const isProd = process.env.NODE_ENV === 'production';
 
 const withPWA = nextPWA({
@@ -14,14 +13,11 @@ const withPWA = nextPWA({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true,
   images: {
-    unoptimized: true,
     domains: ['images.unsplash.com'],
   },
   i18n: nextI18NextConfig.i18n,
-  basePath: isProd ? '/Baayno-Website' : undefined,
-  assetPrefix: isProd ? '/Baayno-Website/' : undefined,
+  output: 'standalone',
 };
 
 export default withPWA(nextConfig);
